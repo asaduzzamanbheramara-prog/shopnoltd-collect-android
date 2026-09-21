366e3b96353c337813ecdb9473b3f2b4e8b8073c
asaduzzamanbheramara-prog <asaduzzaman.bheramara@gmail.com>
2026-09-21 15:22:03 +0600

    fix(android): expose fragments-test to geo unit tests

--- geo/build.gradle.kts
+++ geo/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(libs.dagger)
     kapt(libs.daggerCompiler)
 
-    debugImplementation(project(":fragments-test"))
+    testImplementation(project(":fragments-test"))
 
     testImplementation(project(":androidtest"))
     testImplementation(project(":settings"))
